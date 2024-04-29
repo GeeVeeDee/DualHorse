570008db05a7768d4db3bea36838d770838819da
Rik Tonnelier <tonneri@cronos.be>
2024-04-29 14:03:03 +0200

    Project Setup

---
+++ src/main/java/geeveedee/dualhorse/DualHorse.java
@@ -0,0 +1,17 @@
+package geeveedee.dualhorse;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DualHorse extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
